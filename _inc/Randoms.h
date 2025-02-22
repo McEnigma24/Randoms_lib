5af425adc426951efc1bd22cb7ee46859098c423
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-02-22 22:04:00 +0100

    quick

---
+++ _inc/Randoms.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
+template <typename T>
+struct Random_f
+{
+    static const u64 div_range = 100000;
+
+    static T random_unit_0_to_1() { return (T)(rand() % div_range + 1) / (T)(div_range); }
+
+    static T random_unit_minus_1_to_1() { return ((T)(rand() % div_range + 1) / (T)(div_range / 2)) - (T)(1); }
+};
+
+template <typename T>
+struct Random_int
+{
+    static T distance_between_numbers(T a, T b)
+    {
+        T ret = a - b;
+        return (ret > 0) ? ret : -ret;
+    }
+
+    static T random_int_in_range(T a, T b)
+    {
+        T range = distance_between_numbers(a, b);
+
+        T r = rand() % range;
+
+        T smaller = (a < b) ? a : b;
+
+        return r + smaller;
+    }
+};
--- _src/main.cpp
+++ _src/main.cpp
@@ -1,18 +1,14 @@
 #include "__preprocessor__.h"
 #include "__time_stamp__.h"
 
+#include "Randoms.h"
+
 #ifdef BUILD_EXECUTABLE
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
     CORE::clear_terminal();
     time_stamp("It just works");
-
-    CORE::str::split_string("Hello, World!", ' ');
-    var(CORE::str::to_lower_case("Hello, World!"));
-
-    show_sizeof(test);
-    show_sizeof_many(test, 100);
 
     return 0;
 }
